dec58e0a8baeb986cae12152d4505c47f9c008bf
Dávid Csejtei <csejteidavid90@gmail.com>
2020-03-04 19:12:22 +0100

    WIP home page

--- src/home/components/members.js
+++ src/home/components/members.js
@@ -4,7 +4,7 @@ const Members = () => {
     const members = [
         {
             name: 'Újfalusi Melinda',
-            img: 'meli.png'
+            img: 'empty.png'
         },
         {
             name: 'Csejtei Dávid',
